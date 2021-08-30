e88a69489c6b82c248a2f14d41dbabcd7ed84d3d
jvid <judara2000@gmail.com>
2021-08-30 18:51:14 -0500

    react-router-dom & bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { ProductProvider } from './context/providers/ProductsContext'
+
+import HomePage from './pages/home/HomePage'
+import CustomNavbar from '@/components/ui/Navbar'
+import ProductForm from './pages/products/ProductForm'
+
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Jvid</h1>
-    </div>
+    <Router>
+      <CustomNavbar />
+      <div className="container">
+        <ProductProvider>
+          <Switch>
+            <Route path="/" exact  component={ HomePage } />
+            <Route path="/products/new" component={ ProductForm } />
+          </Switch>
+        </ProductProvider>
+      </div>
+    </Router>
   )
 }
 
