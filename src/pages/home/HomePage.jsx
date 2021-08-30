0bc0dfe0fe82c117f8d7b1f19356e81c61fe6b41
jvid <judara2000@gmail.com>
2021-08-30 18:47:19 -0500

    HomePage & ProductForm pages added

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '@/components/Hero'
+import ProductGrid from '@/components/ProductGrid/ProductGrid'
+
+const HomePage = () => {
+
+  return (
+    <>
+      <Hero />
+      <ProductGrid />
+    </>
+  ) 
+}
+
+export default HomePage
---
+++ src/pages/products/ProductForm.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ProductForm = () => (
+  <>
+    
+  </>
+)
+export default ProductForm
