62d88e367ac43fab6c1ecade46dd5d077186f9de
jvid <judara2000@gmail.com>
2021-08-30 18:49:48 -0500

    Alias to src folder added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+export default ({ mode }) => {
+  // process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [ reactRefresh() ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/'),
+        '@components': path.resolve(__dirname, './src/components/'),
+      }
+    }
+  })
+}
